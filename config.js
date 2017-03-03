9b0c6fce3dd698cc47623d7aa836acfbc001d0f4
Brandy La Voy <brandylavoy@gmail.com>
2017-03-03 03:17:02 +0000

    REPLACE-ME

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                      'mongodb://admin:admin@ds113670.mlab.com:13670/node-restaurants-app-mongoose-tests';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
-	'mongodb://localhost/test-restaurants-app');
+	'mongodb://admin:admin@ds113670.mlab.com:13670/node-restaurants-app-mongoose-tests');
 exports.PORT = process.env.PORT || 8080;
